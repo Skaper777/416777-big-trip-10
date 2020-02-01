d06bfbc615e732cf2535888a4eaecf260776bd40
Skaper777 <skaper777@gmail.com>
2020-02-01 22:46:58 +0300

    dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require(`path`);
 
 module.exports = {
-  mode: `development`,
+  mode: `production`,
   entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
-    path: path.join(__dirname, `public`)
+    path: path.join(__dirname, `docs`)
   },
   devtool: `source-map`,
   devServer: {
-    contentBase: path.join(__dirname, `public`),
+    contentBase: path.join(__dirname, `docs`),
     publicPath: 'http://localhost:8080/',
     compress: true,
     watchContentBase: true
